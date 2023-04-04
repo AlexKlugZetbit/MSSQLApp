8f7d636ad31d303e6805ae322c970f691a82b023
AlexKlugZetbit <96150183+AlexKlugZetbit@users.noreply.github.com>
2023-04-04 23:10:24 +0200

    first commit

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+
+namespace MSSQLApp.Data
+{
+
+    
+    public class AppDbContext : DbContext
+    {
+        public IConfiguration _config { get; set; }
+        public AppDbContext(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_config.GetConnectionString("DatabaseConnection"));
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
